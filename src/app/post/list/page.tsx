a7bb597e06ab4fc5c6dabc01fd8dee3fb445108a
SangWon7242 <gotkdqja@gmail.com>
2025-11-27 12:38:53 +0900

    게시물 리스트 꾸미기

--- src/app/post/list/page.tsx
+++ src/app/post/list/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/app/utils/supabase/server";
 import Link from "next/link";
+import styles from "./List.module.css";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -23,13 +24,26 @@ export default async function Page() {
 
   return (
     <section className="post-list flex w-full flex-1">
-      <div className="inner container mx-auto">
+      <div className="inner container mx-auto flex flex-col gap-2 py-5">
         <h1 className="text-2xl font-bold text-center">내글</h1>
         <nav className="post-menu-wrap">
-          <ul className="grid grid-cols-3 gap-2">
+          <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
             {data.map((post) => (
-              <li key={post.id} className="bg-blue-300">
-                <Link href={`/post/${post.id}`}>{post.title}</Link>
+              <li key={post.id} className={styles["post-list"]}>
+                <Link href={`/post/${post.id}`} className={styles["link-text"]}>
+                  <div className={styles["post-id"]}>{post.id}</div>
+                  <div className={styles["post-info"]}>
+                    <div className={styles["profile-img"]}>^__^</div>
+                    <div className="profile-info">
+                      <div className="post-writer-name text-lg font-bold">
+                        유저1
+                      </div>
+                      <div className="post-writer-date text-sm">
+                        {post.created_at}
+                      </div>
+                    </div>
+                  </div>
+                </Link>
               </li>
             ))}
           </ul>
